0f6b120b281ac1aaea168f147392cefb1791b264
darkflame <darkflame0@sina.com>
2020-02-02 03:02:44 +0800

    使用 ConcurrentDictionary<int, SemaphoreSlim>

--- src/CacheExtensions.cs
+++ src/CacheExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -8,53 +9,44 @@ namespace Microsoft.Extensions.Caching.Memory
 {
     public static class CacheExtensions
     {
-        static private readonly SemaphoreSlim Sem = new SemaphoreSlim(1, 1);
-        public async static Task<TItem> GetOrCreateAtomicAsync<TItem>(this IMemoryCache cache, object key, Func<ICacheEntry, Task<TItem>> factory)
+        private static readonly ConcurrentDictionary<int, SemaphoreSlim> _semaphores = new ConcurrentDictionary<int, SemaphoreSlim>();
+
+        public static async Task<T> GetOrCreateAtomicAsync<T>(this IMemoryCache memoryCache, object key, Func<ICacheEntry, Task<T>> factory)
         {
-            AsyncLazy<TItem> item;
-            await Sem.WaitAsync().ConfigureAwait(false);
+            if (memoryCache.TryGetValue(key, out T value))
+                return value;
+
+            var isOwner = false;
+            var semaphoreKey = (memoryCache, key).GetHashCode();
+            if (!_semaphores.TryGetValue(semaphoreKey, out var semaphore))
+            {
+                SemaphoreSlim? createdSemaphore = null;
+                semaphore = _semaphores.GetOrAdd(semaphoreKey, k => createdSemaphore = new SemaphoreSlim(1)); // Try to add the value, this is not atomic, so multiple semaphores could be created, but just one will be stored!
+
+                if (createdSemaphore != semaphore)
+                    createdSemaphore?.Dispose(); // This semaphore was not the one that made it into the dictionary, will not be used!
+                else
+                    isOwner = true;
+            }
+
+            await semaphore.WaitAsync()
+                           .ConfigureAwait(false); // Await the semaphore!
             try
             {
-                item = cache.GetOrCreate(key, e =>
-                    new AsyncLazy<TItem>(() => factory(e))
-                );
+                if (!memoryCache.TryGetValue(key, out value))
+                {
+                    var entry = memoryCache.CreateEntry(key);
+                    entry.SetValue(value = await factory(entry));
+                    entry.Dispose();
+                }
+
+                return value;
             }
             finally
             {
-                Sem.Release();
-            }
-            try
-            {
-                return await item.Value.ConfigureAwait(false);
-            }
-            catch
-            {
-                cache.Remove(key);
-                throw;
-            }
-        }
-        public static TItem GetOrCreateAtomic<TItem>(this IMemoryCache cache, object key, Func<ICacheEntry, TItem> factory)
-        {
-            Lazy<TItem> item;
-            Sem.Wait();
-            try
-            {
-                item = cache.GetOrCreate(key, e =>
-                    new Lazy<TItem>(() => factory(e))
-                );
-            }
-            finally
-            {
-                Sem.Release();
-            }
-            try
-            {
-                return item.Value;
-            }
-            catch
-            {
-                cache.Remove(key);
-                throw;
+                if (isOwner)
+                    _semaphores.TryRemove(semaphoreKey, out _);
+                semaphore.Release();
             }
         }
     }
