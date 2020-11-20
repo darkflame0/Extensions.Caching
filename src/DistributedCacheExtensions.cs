02c5378be8510a4e36ada31aeb04137849414701
DarkFlame <darkflame0@sina.com>
2020-11-21 02:43:01 +0800

    Distributed:使用redlock

--- src/DistributedCacheExtensions.cs
+++ src/DistributedCacheExtensions.cs
@@ -4,62 +4,63 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using System.Collections.Concurrent;
+using StackExchange.Redis;
+using System.Reflection;
+using System.Linq;
+using RedLockNet.SERedis;
+using RedLockNet;
+using RedLockNet.SERedis.Configuration;
 
 namespace Microsoft.Extensions.Caching.Distributed
 {
     public static class DistributedCacheExtensions
     {
         static readonly JsonSerializerSettings _settings = new JsonSerializerSettings() { MissingMemberHandling = MissingMemberHandling.Error };
+        private static readonly object _locker = new object();
+        private static volatile ConnectionMultiplexer? _connection;
+        private static volatile IDistributedLockFactory? _redlockFactory;
+        private static void InitLock(this IDistributedCache cache)
+        {
+            lock (_locker)
+            {
+                if (_redlockFactory == null)
+                {
+                    _connection = (ConnectionMultiplexer?)(cache.GetType().GetField(nameof(_connection), BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(cache)) ?? throw new ArgumentException("fail to get ConnectionMultiplexer");
+                    _redlockFactory = RedLockFactory.Create(new RedLockMultiplexer[] { _connection });
+                }
+            }
+        }
 
-        private static readonly ConcurrentDictionary<int, SemaphoreSlim> _semaphores = new ConcurrentDictionary<int, SemaphoreSlim>();
-
-        public static async Task<T> GetOrCreateAsync<T>(this IDistributedCache cache, string key, Func<DistributedCacheEntryOptions, Task<T>> factory, Func<T, bool>? notExpiredCheck = null)
+        public static async Task<T> GetOrCreateAsync<T>(this IDistributedCache cache, string key, Func<DistributedCacheEntryOptions, Task<T>> factory, Func<T, bool>? notExpiredCheck = null, CancellationToken cancellationToken = default)
         {
             static bool DefaultCheck(T data) => true;
             notExpiredCheck ??= DefaultCheck;
-            var value = await cache.GetAsync<T>(key);
+            var value = await cache.GetAsync<T>(key, token: cancellationToken);
             if (value != null && notExpiredCheck(value))
                 return value;
-
-            var isOwner = false;
-            var semaphoreKey = (cache, key).GetHashCode();
-            if (!_semaphores.TryGetValue(semaphoreKey, out var semaphore))
+            if (_redlockFactory == null)
             {
-                SemaphoreSlim? createdSemaphore = null;
-                semaphore = _semaphores.GetOrAdd(semaphoreKey, k => createdSemaphore = new SemaphoreSlim(1)); // Try to add the value, this is not atomic, so multiple semaphores could be created, but just one will be stored!
-
-                if (createdSemaphore != semaphore)
-                    createdSemaphore?.Dispose(); // This semaphore was not the one that made it into the dictionary, will not be used!
-                else
-                    isOwner = true;
+                cache.InitLock();
             }
-
-            await semaphore.WaitAsync()
-                           .ConfigureAwait(false); // Await the semaphore!
-            try
+            using var redLock = await _redlockFactory!.CreateLockAsync($"{key}", TimeSpan.FromSeconds(5), TimeSpan.FromMinutes(10), TimeSpan.FromSeconds(0.5), cancellationToken: cancellationToken);
+            if (!redLock.IsAcquired)
             {
-                value = await cache.GetAsync<T>(key);
-                if (value == null || !notExpiredCheck(value))
-                {
-                    var op = new DistributedCacheEntryOptions();
-                    await cache.SetAsync(key, await factory(op), op);
-                }
-
-                return value;
+                throw new Exception($"Failed to get lock: Resource:{redLock.Resource} Status: {redLock.Status}");
             }
-            finally
+            value = await cache.GetAsync<T>(key, token: cancellationToken);
+            if (value == null || !notExpiredCheck(value))
             {
-                if (isOwner)
-                    _semaphores.TryRemove(semaphoreKey, out _);
-                semaphore.Release();
+                var op = new DistributedCacheEntryOptions();
+                await cache.SetAsync(key, value = await factory(op), op, cancellationToken);
             }
+            return value;
         }
-        public static TItem Get<TItem>(this IDistributedCache cache, string key)
+        public static TItem? Get<TItem>(this IDistributedCache cache, string key)
         {
             var str = cache.GetString(key);
             if (str == null)
             {
-                return default!;
+                return default;
             }
             try
             {
@@ -72,15 +73,15 @@ namespace Microsoft.Extensions.Caching.Distributed
             }
             catch (JsonSerializationException)
             {
-                return default!;
+                return default;
             }
         }
-        public static async Task<TItem> GetAsync<TItem>(this IDistributedCache cache, string key, CancellationToken token = default)
+        public static async Task<TItem?> GetAsync<TItem>(this IDistributedCache cache, string key, CancellationToken token = default)
         {
             var str = await cache.GetStringAsync(key, token).ConfigureAwait(false);
             if (str == null)
             {
-                return default!;
+                return default;
             }
             try
             {
@@ -92,33 +93,7 @@ namespace Microsoft.Extensions.Caching.Distributed
             }
             catch (JsonSerializationException)
             {
-                return default!;
-            }
-        }
-        public static TItem GetOrCreate<TItem>(this IDistributedCache cache, string key, Func<DistributedCacheEntryOptions, TItem> factory)
-        {
-            TItem Create()
-            {
-                var item = factory(new DistributedCacheEntryOptions());
-                cache.Set(key, item);
-                return item;
-            }
-            var str = cache.GetString(key);
-            if (str == null)
-            {
-                return Create();
-            }
-            try
-            {
-                if (typeof(TItem).IsTuple())
-                {
-                    return JsonConvert.DeserializeObject<TItem>(str, _settings);
-                }
-                return JsonConvert.DeserializeObject<TItem>(str);
-            }
-            catch (JsonSerializationException)
-            {
-                return Create();
+                return default;
             }
         }
         public static void Set<TItem>(this IDistributedCache cache, string key, TItem value)
